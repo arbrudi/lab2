2aab9bbbae086944cc271b2a2303e55b09a2b7ef
riionah <rh54044@ubt-uni.net>
2024-03-28 23:42:19 +0100

    Update Database.sql
    database part3

--- Database.sql
+++ Database.sql
@@ -41,4 +41,35 @@ CREATE TABLE book_ratings (
   User_ID INT NOT NULL,
   ISBN INT NOT NULL,
   B_Rating INT NOT NULL
+); 
+CREATE TABLE client (
+  User_ID INT NOT NULL PRIMARY KEY,
+  Name VARCHAR(255) NOT NULL,
+  Surname VARCHAR(255) NOT NULL,
+  User_Role VARCHAR(255) NOT NULL,
+  Email VARCHAR(255) NOT NULL,
+  Username VARCHAR(255) NOT NULL,
+  Password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE contact_us (
+  Contact_ID INT NOT NULL PRIMARY KEY,
+  Contact_email VARCHAR(255),
+  Contact_number INT,
+  Contact_address VARCHAR(255),
+  Contact_city VARCHAR(50),
+  Contact_postal_code INT
+);
+
+CREATE TABLE events (
+  Event_ID VARCHAR(255) NOT NULL PRIMARY KEY,
+  Event_image TEXT,
+  Event_description TEXT,
+  Event_date DATE
+);
+
+CREATE TABLE event_participants (
+  Event_ID VARCHAR(255) NOT NULL,
+  User_ID INT NOT NULL,
+  PRIMARY KEY (Event_ID, User_ID)
 );
