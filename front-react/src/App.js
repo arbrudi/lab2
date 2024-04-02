dc9bddc090006d762c1474e785b49efc73d62a8c
riionah <rh54044@ubt-uni.net>
2024-04-02 20:34:48 +0200

    Initializa project using Create React App

---
+++ front-react/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from "./pages/HomePage"; 
+import AboutPage from "./pages/AboutPage"; 
+import BooksPage from "./pages/BooksPage"; 
+import ComicsPage from "./pages/ComicsPage";
+import EventsPage from "./pages/EventsPage";
+import NavBar from './components/NavBar/NavBar';
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+      
+      </div> 
+      <NavBar />
+      <div className="page-body">
+        <Routes>
+          <Route path="/" element={<HomePage />} /> 
+          <Route path="/about" element={<AboutPage />} /> 
+          <Route path="/books" element={<BooksPage />} /> 
+          <Route path="/comics" element={<ComicsPage />} /> 
+          <Route path="/Eevents" element={<EventsPage />} /> 
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
