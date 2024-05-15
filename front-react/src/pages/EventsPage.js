855437c878ae15a89541a273d2f9f31222842bb6
riionah <rh54044@ubt-uni.net>
2024-05-15 17:38:58 +0200

    Eventpage css

--- front-react/src/pages/EventsPage.js
+++ front-react/src/pages/EventsPage.js
@@ -1,10 +1,45 @@
-import React from "react"; 
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import { Link } from "react-router-dom";
+import './pages_css/EventsList.css'; // Assuming this CSS file contains styles for the Event component
 
+const EventsPage = () => {
+  const [events, setEvents] = useState([]);
+ 
 
-const EventsPage = () =>{
-    return(
-        <h1> This is the Events Page!</h1>
-    );
-}
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await axios.get("/admin/events");
+        setEvents(response.data);
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
+    };
 
-export default EventsPage; 
+    fetchEvents();
+  }, []);
+
+ 
+
+ 
+  return (
+    <div className='container'>
+     
+      <div className='events-list'>
+        {events.map((event) => (
+          <div key={event.Event_ID} className="event-item">
+            <img src={event.Event_image} alt="Event" className="event-image" />
+            <div className="event-details">
+              <p className="event-description">{event.Event_description}</p>
+              <p className="event-date">{event.Event_date}</p>
+              <Link to={`/events/${event.Event_ID}`} className="event-link">View Details</Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EventsPage;
