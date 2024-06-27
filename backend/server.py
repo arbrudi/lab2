3f0a0cdb573cc24923f08fbc8bf3e0c6da722730
Arb <ar51896@ubt-uni.net>
2024-06-27 20:33:43 +0200

    fix

--- backend/server.py
+++ backend/server.py
@@ -30,7 +30,7 @@ def create_app():
 
     conn = 'mssql+pyodbc:///?odbc_connect=' + \
            'DRIVER={ODBC Driver 17 for SQL Server};' + \
-           'SERVER=ERIS;' + \
+           'SERVER=LAPTOP-TQGV5751;' + \
            'DATABASE=lab2;' + \
            'Trusted_Connection=yes;'
     app.config['SQLALCHEMY_DATABASE_URI'] = conn
